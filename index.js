375eafdf63076f7c812ddf3bddeff75fb87bb55e
rdmw691@gmail.com <rdmw691@gmail.com>
2018-07-11 18:22:48 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,40 @@
+function add(x, y) {
+  return x + y
+}
+
+add (1, 80)
+
+function subtract(x, y) {
+  return x - y
+}
+
+subtract (60, 40)
+
+function multiply(x, y) {
+  return x * y
+}
+
+multiply(2, 3.4)
+
+function divide(x, y) {
+  return x / y
+}
+
+divide(5.0, 2.5)
+
+function inc(x) {
+  return x + 1
+}
+
+number++
+
+function dec(x) {
+  return x - 1
+}
+
+number--
+
+function makeInt(n) {
+  return 'n'
+}
+
